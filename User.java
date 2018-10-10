824388d0f6a50d2dee69f6f229e8da3e91d3d1ee
zjwzjwzjwzjw <18937076190@189.cn>
2018-10-10 17:22:18 +0800

    first commit

---
+++ User.java
@@ -0,0 +1,67 @@
+package entity;
+
+import java.util.Date;
+
+/**
+ * Created by user on 2018/9/27.
+ */
+public class User {
+    private int id;
+    private String name;
+    private String password;
+    private Date createTime;
+    private Date lastLoginTime;
+    public User() {
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(Date lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", createTime=" + createTime +
+                ", lastLoginTime=" + lastLoginTime +
+                '}';
+    }
+}
